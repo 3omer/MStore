55d03234db1c41579b3f57aff95e550c3e978a6b
3omer <omarosmandev@gmail.com>
2021-07-25 14:56:09 +0200

    rename admin to main

---
+++ src/app/app.routing.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+
+const routes: Routes =[
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  }, {
+    path: '',
+    component: MainLayoutComponent,
+    children: [{
+      path: '',
+      loadChildren: () => import('./layouts/main-layout/main-layout.module').then(m => m.MainLayoutModule)
+    }]
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+       useHash: true
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
